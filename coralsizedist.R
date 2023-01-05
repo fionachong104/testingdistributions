3ce0ecac6e5ef8c884979476138ab2330d1805aa
FIONA CHONG <F.Chong-2019@hull.ac.uk>
2023-01-05 16:14:26 +0000

    coralsizedist.R: Fomatting changes - only one x and one y axis label for all the plots. Y axis now cap at 3000

--- coralsizedist.R
+++ coralsizedist.R
@@ -29,10 +29,7 @@ for(i in 1:nsites){
   bML <- mle_b(Site == s, x = siteinput$Area, log_x = sitedata$log.Area, sum_log_x = siteinput$sum.log.Area,
                          x_min = siteinput$min.Area, x_max = siteinput$max.Area)
   PLB.bMLE.site.b[i] <- bML[[1]] 
-  #PLB.minLL.site.b <- PLB.return.site[[2]]
-  #PLB.minNegLL.site.b <- PLB.minLL.site.b$minimum
   MSPLB.bMLE.site.b[i] <- estimatebMSBPL(x = sitedata$Area, w = w, v = v)$minimum
-  
   #x <- sitedata$Area
   sitex = seq(min(sitedata$Area), max(sitedata$Area), length = 1000)
   sitey.PLB = (1 - pPLB(x = sitex, b = PLB.bMLE.site.b[i], xmin = min(sitex),
@@ -42,9 +39,7 @@ for(i in 1:nsites){
   siteb_plot[[i]] <- ggplot() +
     geom_point(aes_(x = (sort(sitedata$Area, decreasing=TRUE)), y = (1:length(sitedata$Area))),
                color = "cadetblue", size = 2, alpha = 0.3) +
-    xlab(expression(paste("Colony area, ", italic("x"), ~(cm^2)))) +
-    ylab(expression(paste("Number of colonies with sizes", " ">=" ", italic("x"), "    "))) +
-    scale_y_continuous(trans = 'log10', breaks = c(1,10,100), # currently capping at 100 because it is plotting Woolgoolga only; annoyingly the y axis doesnt scale but this will not be a problem when we plot the actual data and fit for each site - we will need to re-add 500 and 3000
+    scale_y_continuous(trans = 'log10', breaks = c(1,10,100,500,3000), 
                        limits = c(0.25, max(table(oneyeardf$Site)))) +
     scale_x_continuous(trans = 'log10', breaks = c(0,1,5,10,100,1000,10000),
                        limits = range(oneyeardf$Area))+
@@ -53,6 +48,13 @@ for(i in 1:nsites){
     annotate("text", x = 5, y = 10, label = s) +
     annotate("text", x = 5, y = 3, label = bquote(paste(italic(b)[PLB]==.(round(PLB.bMLE.site.b[i],2))))) +
     annotate("text", x = 5, y = 1, label = bquote(paste(italic(b)[MSBPL]==.(round(MSPLB.bMLE.site.b[i],2))))) +
-    theme_classic()
+    theme_classic() + 
+    theme(axis.title = element_blank())
 }
-do.call(grid.arrange, siteb_plot)
+
+leftlabel <- grid::textGrob(expression(paste("Number of colonies with sizes", " ">=" ", italic("x"), "    ")), rot = 90)
+bottomlabel <- grid::textGrob(expression(paste("Colony area, ", italic("x"), ~(cm^2))))
+grid.arrange(grobs = siteb_plot, ncol = 4, 
+             left = leftlabel,
+             bottom = bottomlabel)
+
