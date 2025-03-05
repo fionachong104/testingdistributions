25a97b5789064f930d9311377feb6c4fd3de6053
Matthew Spencer <m.spencer@liverpool.ac.uk>
2025-03-05 10:55:17 +0000

    add plussampledlognormal.R

---
+++ plussampledlognormal.R
@@ -0,0 +1,74 @@
+rm(list = ls())
+library(RColorBrewer)
+
+source("coralsizedistfuncs.R")
+
+# images dimensions from CANON camera (w x v = 3648 x 2736)
+# 10 cm = roughly 350 pixels; 1 m = 3500; 1 cm = 35; so dividing by 35 to see what that translates to in cm x cm
+w <- 3648/35
+v <- 2736/35
+
+r <- 10 #fixed r for now
+n <- 1e2
+nplot <- 1e2
+mycolors <- brewer.pal(3, "Dark2")
+alpha <- runif(n = n, min = 0 - r, max = w + r) #(alpha, beta) uniform random points in plus-sampling window
+beta <- runif(n = n, min = 0 - r, max = v + r)
+par(mfrow = c(1, 1))
+plot(c(0 - r, w + r), c(0 - r, v + r), type = "n", asp = 1, xlab = "", ylab = "")
+plotframe(w = w, v = v)
+istrunc <- intrunc(alpha = alpha, beta = beta, r = r, w = w, v = v)
+for(i in 1:nplot){#drawing all the circles can be slow so make nplot not too large
+  plotcircle(alpha = alpha[i], beta = beta[i], r = r[i], isinframe = istrunc[i])
+}
+
+#xmaxminus <- pi / 4 * v^2 #max area of circle that can fit in window
+#n <- 1e4 #number of circles
+#nplot <- 100 #number to plot
+#mu <- 5 #mean log area
+#sigma <- 1 #sd log area
+#x <- rlnorm(n = n, meanlog = mu, sdlog = sigma) #simulated areas from lognormal
+
+#r <- radiusfromarea(x) #colony radii (assume circles)
+
+#par(mfrow = c(3, 3))
+#plot(c(0, w), c(0, v), type = "n", asp = 1, xlab = "", ylab = "")
+#plotframe(w = w, v = v)
+#isinframe <- inframe(alpha = alpha, beta = beta, r = r, w = w, v = v)
+#for(i in 1:nplot){#drawing all the circles can be slow so make nplot not too large
+#  plotcircle(alpha = alpha[i], beta = beta[i], r = r[i], isinframe = isinframe[i])
+#}
+
+#hist(x, freq = FALSE, main = "lognormal", breaks = 100, xlab = expression(italic(x)), ylab = "density")
+#xseq <- seq(from = min(x), to = max(x), length.out = 1e4)
+#fx <- dlnorm(x = xseq, meanlog = mu, sdlog = sigma)
+#lines(xseq, fx, col = mycolors[1], lwd = 2, lty = "solid") #density curve for lognormal
+#legend("topright", bty = "n", lwd = 2, lty = c("solid"), col = c(mycolors[1]), legend = c("calculated"))
+
+#hist(x[isinframe], freq = FALSE, main = "minus-sampled lognormal", breaks = 100, xlab = expression(italic(x)), ylab = "density")
+#xseqMS <- seq(from = min(x), to = max(x), length.out = 1e4)
+#fxMS <- dMSlnorm(x = xseqMS, mu = mu, sigma = sigma, v = v, w = w)
+#lines(xseqMS, fxMS, col = mycolors[2], lwd = 2, lty = "dashed") #density curve for minus-sampled lognormal
+#legend("topright", bty = "n", lwd = 2, lty = c("dashed"), col = c(mycolors[2]), legend = c("calculated"))
+
+#plot(log(xseq), log(fx), type = "l", lwd = 2, lty = "solid", col = mycolors[1], xlab = expression(log(italic(x))), ylab = "log density")
+#lines(log(xseqMS), log(fxMS), col = mycolors[2], lwd = 2, lty = "dashed")
+#legend("bottomleft", bty = "n", lwd = 2, lty = c("solid", "dashed"), col = c(mycolors[1], mycolors[2]), legend = c("lognormal", "minus-sampled lognormal"))
+
+#thetaML <- estimateMSlnorm(x = x[isinframe], w = w, v = v)
+#plotnegloglikMSlnorm(x = x[isinframe], w = w, v = v, mu = mu, sigma = sigma, muhat = thetaML$par[1], sigmahat = thetaML$par[2]) #contours of negative log likelihood
+#print("Parameter estimates:")
+#print(thetaML$par)
+
+#ninframe <- length(x[isinframe])
+#sitex.MSlnorm <- seq(min(x[isinframe]), max(x[isinframe]), length = 1000)
+#sitey.MSlnorm <- (1 - sapply(sitex.MSlnorm, FUN = FMSlnorm, mu = mu, sigma = sigma, w = w, v = v)) * ninframe
+#plot(sort(x[isinframe], decreasing=TRUE), 1:ninframe, xlab = expression(italic(x)), ylab = expression(italic(S(x)*n)[inframe]))
+#lines(sitex.MSlnorm, sitey.MSlnorm)
+
+#minus-sampled lognormal qqplot
+#qqplot(FMSlnorminv(u = ppoints(ninframe), mu = mu, sigma = sigma, w = w, v = v), x[isinframe], xlab = "theoretical quantiles", ylab = "sample quantiles", main = "minus-sampled lognormal Q-Q plot")
+
+#qqline(x[isinframe], distribution = function(p){
+#  FMSlnorminv(p, mu = mu, sigma = sigma, w = w, v = v)
+#})
