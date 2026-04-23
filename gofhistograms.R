1844f20930f60fd7b6df27d968853963af38879a
Fiona Chong <tvkx991@leeds.ac.uk>
2026-04-23 12:32:06 +0100

    gofhistograms.R: make histograms of GOF test p values gofall.csv: contains GOF test statistics

---
+++ gofhistograms.R
@@ -0,0 +1,68 @@
+rm(list=ls())
+
+library(ggplot2)
+library(dplyr)
+
+gofall <- read.csv("gofall.csv")
+
+custom_breaks <- seq(-0.15, 1.25, by = 0.2)
+
+
+ggplot(gofall, aes(x = lognormP, fill = Taxa)) +
+  geom_histogram(breaks = custom_breaks,  alpha = 0.8) +
+  stat_bin(
+    aes(label = after_stat(if_else(count > 0, as.character(count), "")), group = 1),
+    geom = "text",
+    breaks = custom_breaks,
+    vjust = -1
+  ) +
+  scale_x_continuous(breaks = custom_breaks, limits = c(-0.2, 1.25)) +
+  scale_fill_brewer(palette = "Dark2") +
+  geom_vline(xintercept = 0.05) +
+  labs(x = "Log-normal GOF test P-value", y = "Number of observations") +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ ggplot(gofall, aes(x = BPLP, fill = Taxa)) +
+  geom_histogram(breaks = custom_breaks, alpha = 0.8) +
+  stat_bin(
+    aes(label = after_stat(if_else(count > 0, as.character(count), "")), group = 1),
+    geom = "text",
+    breaks = custom_breaks,
+    vjust = -1
+  ) +
+  scale_x_continuous(breaks = custom_breaks, limits = c(-0.2, 1.25)) +
+  scale_fill_brewer(palette = "Dark2") +
+  geom_vline(xintercept = 0.05) +
+  labs(x = "Bounded Power Law GOF test P-value", y = "Number of observations") +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# corals only for minus sampled data
+ggplot(gofall, aes(x = MSBPLP)) +
+  geom_histogram(breaks = custom_breaks, alpha = .4) +
+  stat_bin(
+    aes(label = after_stat(if_else(count > 0, as.character(count), "")), group = 1),
+    geom = "text",
+    breaks = custom_breaks,
+    vjust = -1
+  ) +
+  scale_x_continuous(breaks = custom_breaks, limits = c(-0.2, 1.25)) +
+  geom_vline(xintercept = 0.05) +
+  labs(x = "Minus-sampled Bounded Power Law GOF test P-value", y = "Number of observations") +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(gofall, aes(x = MSlognormP)) +
+  geom_histogram(breaks = custom_breaks, alpha = .4) +
+  stat_bin(
+    aes(label = after_stat(if_else(count > 0, as.character(count), "")), group = 1),
+    geom = "text",
+    breaks = custom_breaks,
+    vjust = -1
+  ) +
+  scale_x_continuous(breaks = custom_breaks, limits = c(-0.2, 1.25)) +
+  geom_vline(xintercept = 0.05) +
+  labs(x = "Minus-sampled Log-normal GOF test P-value", y = "Number of observations") +
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
