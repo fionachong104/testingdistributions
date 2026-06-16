d2cc01348547a00f2d81d4245027990a99e08973
Matthew Spencer <m.spencer@liverpool.ac.uk>
2026-06-16 17:06:49 +0100

    coralsizedistfuncs.R, plotFisherapprox(): tidy up and use in gofnull.R

--- gofnull.R
+++ gofnull.R
@@ -3,5 +3,9 @@ source("coralsizedistfuncs.R")
 
 gof <- read.table(file = "gofall.csv", header = TRUE, sep = ",")
 
-plotFisherapprox(X2 = gof$MSlognormX2, df = gof$MSlognormdf)
+par(mfrow = c(2, 2))
+plotFisherapprox(X2 = gof$MSlognormX2, df = gof$MSlognormdf, label = "a: minus-sampled lognormal")
+plotFisherapprox(X2 = gof$lognormX2, df = gof$lognormdf, label = "b: lognormal")
+plotFisherapprox(X2 = gof$MSBPLX2, df = gof$MSBPLdf, label = "c: minus-sampled bounded power law")
+plotFisherapprox(X2 = gof$BPLX2, df = gof$BPLdf, label = "d: bounded power law")
      
